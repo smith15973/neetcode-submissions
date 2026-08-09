344007bf169b219b8be62cb01acd00fc5e5edfe1
smith15973 <156469677+smith15973@users.noreply.github.com>
2026-08-09 08:38:48 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        word = ""
+        for s in strs:
+            size = len(s)
+            word = word + (f"{size}#{s}")
+            print(s)
+        return word
+
+    def decode(self, s: str) -> List[str]:
+        message = []
+        pointer = 0
+        for index, c in enumerate(s):
+            if pointer < index and s[index] == '#':
+                count = int(s[pointer:index])
+                # print("COUNT", count)
+                message.append(s[index+1:index+count+1])
+                # print("MESSAGE", message)
+                pointer = index+count+1
+
+        return message
